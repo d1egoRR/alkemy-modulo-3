4072cd1b0e19ba89cfe149f6e695906b23da72c1
d1egoRR <diegoriquelme81@gmail.com>
2023-04-06 16:39:25 -0300

    Modulo 3 - Clase 9

--- sample/views.py
+++ sample/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import HttpResponse, render
 
+from sample.models import Marca, Producto
+
 
 def saludar(request):
     return HttpResponse("Hola Mundo!! Esta es mi primer view")
@@ -13,3 +15,34 @@ def ejemplo_template(request):
     }
 
     return render(request, "sample/ejemplo.html", context)
+
+
+def listado_productos(request):
+    productos = Producto.objects.all()
+
+    return render(
+        request,
+        "sample/listado_productos.html",
+        {"productos": productos}
+    )
+
+
+def nuevo_producto(request):
+    marcas = Marca.objects.all()
+
+    context = {
+        "marcas": marcas
+    }
+
+    if request.POST:
+        nombre = request.POST['nombre']
+        precio = request.POST['precio']
+        marca_id = request.POST['marca']
+
+        Producto.objects.create(
+            nombre=nombre,
+            precio=precio,
+            marca_id=marca_id
+        )
+
+    return render(request, "sample/nuevo_producto.html", context)
